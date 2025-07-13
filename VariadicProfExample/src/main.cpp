61acf262f2e1c98f7ef4c9c18300c9c7422328ae
Hrishikesh Karande <hrishikeshkarande1997@gmail.com>
2025-07-13 15:29:13 +0200

    Fixing Indents

--- VariadicProfExample/src/main.cpp
+++ VariadicProfExample/src/main.cpp
@@ -1,24 +1,29 @@
 #include <cstdarg>
 #include <iostream>
 
-void myPrint(const char * format, ...) {
- va_list args;
- va_start(args, format);
- while (*format != '\0') {
- int i = va_arg(args, int);
- if (*format == 'd') {
- std::cout << 'i' << i << ' ';
- } else if (*format == 'c') {
- std::cout << 'z' << (char)i << ' ';
- }
- ++format;
- }
- va_end(args);
+void myPrint(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    while (*format != '\0')
+    {
+        int i = va_arg(args, int);
+        if (*format == 'd')
+        {
+            std::cout << 'i' << i << ' ';
+        }
+        else if (*format == 'c')
+        {
+            std::cout << 'z' << (char)i << ' ';
+        }
+        ++format;
+    }
+    va_end(args);
 }
 
-
-int main() {
- myPrint("dcd", 3, 'a', 14);
- myPrint("cc", 'c', 'd');
- std::cout << '\n';
+int main()
+{
+    myPrint("dcd", 3, 'a', 14);
+    myPrint("cc", 'c', 'd');
+    std::cout << '\n';
 }
