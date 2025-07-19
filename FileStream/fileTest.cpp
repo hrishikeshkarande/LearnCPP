ce96caab12c042291a6100be268bd2d46047fc67
Hrishikesh Karande <hrishikeshkarande1997@gmail.com>
2025-07-19 15:06:32 +0200

    Reading from a file and displaying its content on the console

---
+++ FileStream/fileTest.cpp
@@ -0,0 +1,1 @@
+//Hi I am Hrishikesh, I am Learning CPP.
---
+++ FileStream/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream> // input file stream class std::ifstream
+
+int main() {
+ std::ifstream myFile("fileTest.cpp"); // initialize with constructor
+
+ auto c = ' ';
+ // get a character from the file, move to next, and output it to the terminal
+ while (myFile.get(c)) std::cout << c;
+ std::cout << '\n';
+}
